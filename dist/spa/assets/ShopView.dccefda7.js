26ce04484fab58a9e7645f8174e209746b4830b6
muro472 <keklol472@gmail.com>
2023-07-08 23:43:04 +0300

    new build

--- dist/spa/assets/ShopView.dccefda7.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,G as r,I as c}from"./index.25ce80c2.js";const o={};function n(t,a){return r(),c("div",null,"hello")}var l=e(o,[["render",n]]);export{l as default};
---
+++ dist/spa/assets/use-spinner.efa7212a.js
@@ -0,0 +1,1 @@
+import{c as i,N as s}from"./index.13386955.js";const t={size:{type:[Number,String],default:"1em"},color:String};function r(e){return{cSize:i(()=>e.size in s?`${s[e.size]}px`:e.size),classes:i(()=>"q-spinner"+(e.color?` text-${e.color}`:""))}}export{r as a,t as u};
